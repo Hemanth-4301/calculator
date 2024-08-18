f2974e8fd9d55dfa9676df827296ef5094705e6b
Hemanth-4301 <hemanthachar526@gmail.com>
2024-08-18 23:06:48 +0530

    deploy react to github

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import { useState } from "react";
 function App() {
+  const [inValue, setInValue] = useState("");
+
+  function display(value) {
+    setInValue(inValue + value);
+  }
+
+  function clear() {
+    setInValue("");
+  }
+
+  function evaluate() {
+    const ans = eval(inValue);
+    setInValue(ans);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="heading  text-xl ">Calculator</h1>
+
+      <form className="calculator ">
+        <input
+          className="value"
+          value={inValue}
+          onChange={(e) => setInValue(e.target.value)}
+        />
+        <span className="num clear hover:text-red-700" onClick={clear}>
+          Clear
+        </span>
+        <span onClick={() => display("%")}>%</span>
+        <span onClick={() => display("/")}>/</span>
+        <span onClick={() => display("7")}>7</span>
+        <span onClick={() => display("8")}>8</span>
+        <span onClick={() => display("9")}>9</span>
+        <span onClick={() => display("*")}>x</span>
+        <span onClick={() => display("4")}>4</span>
+        <span onClick={() => display("5")}>5</span>
+        <span onClick={() => display("6")}>6</span>
+        <span onClick={() => display("-")}>-</span>
+        <span onClick={() => display("1")}>1</span>
+        <span onClick={() => display("2")}>2</span>
+        <span onClick={() => display("3")}>3</span>
+        <span onClick={() => display("+")}>+</span>
+        <span onClick={() => display("0")}>0</span>
+        <span onClick={() => display("00")}>00</span>
+        <span onClick={() => display(".")}>.</span>
+        <span onClick={evaluate}>=</span>
+      </form>
+    </>
   );
 }
 
